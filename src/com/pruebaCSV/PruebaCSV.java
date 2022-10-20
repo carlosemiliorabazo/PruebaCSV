5a4fc08b60e0cc4abb7006dbd4323525ef206493
Carlos Rabazo <carlosemilio.rabazo@ufv.es>
2022-10-20 20:41:56 +0200

    Primer commit

---
+++ src/com/pruebaCSV/PruebaCSV.java
@@ -0,0 +1,14 @@
+package com.pruebaCSV;
+/*
+Ejemplo muy sencillo de acceso a un fichero .CSV con datos de pokemon
+No utiliza ningún gestor de dependencias
+ */
+public class PruebaCSV {
+    /**
+     * @author carlos rabazo
+     */
+    public static void main(String[] args) {
+        LectorCSV miLector = new LectorCSV();
+        miLector.leeCSV("./datos/pokemon.csv");
+    }
+}
